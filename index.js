7546e7e8e9a26b2ce1e51d5b7d18316fca2b4147
Brian Ressler <137207208+bressler95tops@users.noreply.github.com>
2024-09-25 22:57:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-console.log("Starting Host...");
-    
-    var receiver = document.getElementById('receiver').contentWindow;
+window.addEventListener('message', receiveMessage, false);
+function receiveMessage(event){
+    console.log("Received something from embedded iFrame" + event.origin);
+    if(event.origin!=="https://bressler95tops.github.io")
+    return;
+
+}
+
+var receiver = document.getElementById('receiver').contentWindow;
 
     // Get a reference to the 'Send Message' button.
     var btn = document.getElementById('send');
