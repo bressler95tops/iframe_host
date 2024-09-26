90dfec87d65d97ecaafe4e571dc06f4d0af550b8
Brian Ressler <137207208+bressler95tops@users.noreply.github.com>
2024-09-25 21:42:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload=function(){
     var messageEle=document.getElementById('message');
     function receiveMessage(e){
-        if(e.origin!=="http://correct-domain.com")
+        if(e.origin!=="https://iframehost.pages.dev")
         return;
         messageEle.innerHTML="Message Received: "+e.data;
     }
