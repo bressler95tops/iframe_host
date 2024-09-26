21196f89195d25cf45b066e04e8a7927fc07286b
Brian Ressler <137207208+bressler95tops@users.noreply.github.com>
2024-09-25 23:13:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ function receiveMessage(event){
     console.log("Received something from embedded iFrame: " + event.origin);
     if(event.origin !== "https://iframehost.pages.dev")
     return;
+    console.log("Setting value to local storage: " + event.data);
     localStorage.setItem("cookie_value", event.data);
 }
 
 // A function to handle sending messages.
 function init_iframe() {
+    console.log("Attempting to initialize iframe value with local storage...");
     const cookie_value = localStorage.getItem("cookie_value");
     receiver.postMessage(cookie_value, '*');
 }
